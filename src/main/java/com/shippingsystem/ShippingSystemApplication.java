b31f13c3c76d1208736e5b8ed28135d796187ec0
saucecat114514 <1972372765@qq.com>
2025-06-09 10:56:53 +0800

    Initial commit: Spring Boot Shipping System

---
+++ src/main/java/com/shippingsystem/ShippingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shippingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShippingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shippingsystem/ShippingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shippingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
